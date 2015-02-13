8faa9fadf9d11f32ff39e557d74c45272d847f65
pol_kos <pol_kos@ukr.net>
2015-02-13 21:24:43 +0200

    session storage added and script optimized

--- vsHandyStorage.js
+++ vsHandyStorage.js
@@ -1,60 +1,99 @@
 angular.module('vsHandyStorage', ['ngStorage'])
-    .factory('vsStorageFactory', ['$localStorage', function($localStorage) {
+    .factory('vsStorageService', ['$localStorage', '$sessionStorage', function($localStorage, $sessionStorage) {
 		function createScope(scopeName) {
 		    var scope = {};
 		    scope[scopeName] = {};
 			return scope;
 		}		
-		function initStorage(scope) {
-		    $localStorage.$default(scope);
+		
+		function create(storage, name) {
+		    if (name in storage)
+			    return storage[name];	    
+			storage.$default(createScope(name));
+			return storage[name];
 		}
 		
-		function create(name) {
-		    if (name in $localStorage)
-			    return $localStorage[name];
-		    
-			var scope = createScope(name);
-			initStorage(scope);
-			
-			return $localStorage[name];
+		function createLocal(name) {
+		    var localStorage = create($localStorage, name);
+			return localStorage;
+		}
+		function createSession(name) {
+		    var sessionStorage = create($sessionStorage, name);
+			return sessionStorage;
 		}
 		
 		return {
-		    create: create
+		    createLocal: createLocal,
+		    createSession: createSession
 		};
-	 }])	 
-    .directive('vsLocalStorage', ['vsStorageFactory', function(vsStorageFactory) {
+	 }])
+    .factory('vsAbstractStorageModel', function() {
+        function AbstractStorage() {
+		    this.storage = {};
+            this.put = function (key, value) {
+                this.storage[key] = value;
+            };
+            this.get = function (key) {
+                return this.storage[key];
+            };
+		}
+		
+		return new AbstractStorage();
+    })
+	.factory('vsLocalStorageModel', ['vsAbstractStorageModel', 'vsStorageService', function(vsAbstractStorageModel, vsStorageService) {
+        function LocalStorage(name) {        
+            this.storage = vsStorageService.createLocal(name);
+        }
+        LocalStorage.prototype = vsAbstractStorageModel;
+    
+        return LocalStorage;
+    }])
+    .factory('vsSessionStorageModel', ['vsAbstractStorageModel', 'vsStorageService', function(vsAbstractStorageModel, vsStorageService) {
+        function SessionStorage(name) {        
+            this.storage = vsStorageService.createSession(name);
+        }
+        SessionStorage.prototype = vsAbstractStorageModel;
+    
+        return SessionStorage;
+    }])		
+    .directive('vsLocalStorage', ['vsLocalStorageModel', function(vsLocalStorageModel) {
         return {
             restrict: 'A',
             controller: function($scope, $element, $attrs) {
-                var storage = vsStorageFactory.create($attrs.vsLocalStorage);
-			
-				this.put = function(key, value) {
-				    storage[key] = value;
-                };
-				this.get = function(key) {
-				    return storage[key];
-                };
+                var storage = new vsLocalStorageModel($attrs.vsLocalStorage);    
+				
+				this.storage = storage;
+            }
+        }
+    }])
+	.directive('vsSessionStorage', ['vsSessionStorageModel', function(vsSessionStorageModel) {
+        return {
+            restrict: 'A',
+            controller: function($scope, $element, $attrs) {
+                var storage = new vsSessionStorageModel($attrs.vsSessionStorage);    
+				
+				this.storage = storage;
             }
         }
     }])
 	.directive('vsLinkStorage', function() {
         return {
             restrict: 'A',
-            require: ['^vsLocalStorage', 'ngModel'],
+            require: ['?^^vsLocalStorage', '?^^vsSessionStorage', 'ngModel'],
             link:function(scope, element, attrs, controllers) {
-                var vsLocalStorage = controllers[0];
-                var ngModel = controllers[1];
+			    var storageCtrl = controllers[0] || controllers[1];
+                var storage = storageCtrl.storage;
+                var ngModel = controllers[2]; 
 	
-                var initValue = vsLocalStorage.get(attrs.name);
+                var initValue = storage.get(attrs.name);
                 if (initValue !== undefined) {
                     ngModel.$setViewValue(initValue);
-                    ngModel.$render();
+                    ngModel.$render();  
                 }                
 				
                 scope.$watch(attrs.ngModel, function() {
                     if(ngModel.$dirty) {
- 					   vsLocalStorage.put(attrs.name, ngModel.$modelValue);
+ 					   storage.put(attrs.name, ngModel.$modelValue);
                     }
                 });
             } 
